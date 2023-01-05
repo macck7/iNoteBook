afb98bc80f6ad0188e19ce899d5f20351129a2bb
Mohit Kumar Gupta <49000628+macck7@users.noreply.github.com>
2023-01-05 23:12:14 +0530

    Update NoteState.js

--- client/src/context/notes/NoteState.js
+++ client/src/context/notes/NoteState.js
@@ -2,7 +2,7 @@ import NoteContext from "./noteContext"
 import React, { useState } from 'react'
 
 const NoteState = (props) => {
-    const host = "http://localhost:5000"
+    const host = "https://inotebook-rqnr.onrender.com"
     const notesIntial = []
 
     const [notes, setNotes] = useState(notesIntial)
